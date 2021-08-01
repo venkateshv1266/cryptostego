286a36d4ff451701083575621dca47ee813c22e1
Venkatesh V <venkateshv1266@gmail.com>
2021-08-01 22:18:59 +0530

    changed

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ import imageRouter from './imageRouter.js';
 import userRouter from './userRouter.js';
 import sendEmailRouter from './sendEmailRouter.js';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 //To read the content of .env file
 dotenv.config();
 
@@ -15,6 +20,9 @@ const port = process.env.PORT || 5000;
 //Must include this middleware in order to parse the data in frontend.
 app.use(express.json({limit: '500mb'})); 
 app.use(express.urlencoded({ extended: true, limit: '500mb'}));
+
+app.use(express.static(path.join(__dirname, '/frontend/build'))); //to serve the frontend files
+app.get('*', (req,res) => res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
 
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
@@ -30,9 +38,9 @@ app.get('/api/data', (req,res) => {
     res.send(data);
 });
 
-app.get('/', (req, res) => {
-    res.status(200).send(`Server Connected at PORT ${process.env.PORT}`);
-});
+// app.get('/', (req, res) => {
+//     res.status(200).send(`Server Connected at PORT ${process.env.PORT}`);
+// });
 
 app.listen(port, () => {
     console.log(`Listening to port at ${port}`);
